876ad5d6190591bfe64b134d2e24be93e560f7ce
Michal Turek <turek@avast.com>
2016-01-15 08:47:19 +0100

    [BRG-355] Fix build-time warnings from javadoc.

--- api/src/main/java/com/avast/metrics/api/Monitor.java
+++ api/src/main/java/com/avast/metrics/api/Monitor.java
@@ -8,11 +8,19 @@ public interface Monitor extends AutoCloseable {
      * Returns a new instance with the given name. This method may be called multiple
      * times - the resulting monitor will remember all its names (basically creating
      * a hierarchy of names).
+     *
+     * @param name name of the next sub-level
+     * @return new monitor derived from this one
      */
     Monitor named(String name);
 
     /**
      * Returns a new instance with the given names appended.
+     *
+     * @param name1       name of the next sub-level
+     * @param name2       name of the next sub-sub-level
+     * @param restOfNames names of the other sub-levels
+     * @return new monitor derived from this one
      */
     Monitor named(String name1, String name2, String... restOfNames);
 
