eccc94fc44888dda1ee0ad942e56c748a67f06db
Tomas Herman <hermant@avast.com>
2016-07-22 11:19:16 +0200

    implemented disabling jmx

--- dropwizard/src/main/java/com/avast/metrics/dropwizard/JmxMetricsMonitor.java
+++ dropwizard/src/main/java/com/avast/metrics/dropwizard/JmxMetricsMonitor.java
@@ -1,14 +1,22 @@
 package com.avast.metrics.dropwizard;
 
-import com.avast.metrics.api.Monitor;
-import com.avast.metrics.api.Naming;
+import com.avast.metrics.api.*;
+import com.avast.metrics.test.NoOpMonitor;
 import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.ObjectNameFactory;
 
+import java.util.Objects;
+import java.util.function.Supplier;
+
+
+/**
+ *
+ */
 public class JmxMetricsMonitor extends MetricsMonitor {
 
     private final JmxReporter reporter;
+    private final Monitor overrideMonitor;
 
     public JmxMetricsMonitor(String domain) {
         this(domain, new MetricRegistry(), Naming.defaultNaming());
@@ -30,11 +38,26 @@ public class JmxMetricsMonitor extends MetricsMonitor {
             .createsObjectNamesWith(objectNameFactory)
             .build();
         this.reporter.start();
+        this.overrideMonitor = overrideMonitor();
     }
 
     private JmxMetricsMonitor(JmxMetricsMonitor original, String... names) {
         super(original, names);
         this.reporter = original.reporter;
+        this.overrideMonitor = overrideMonitor();
+    }
+
+    private Monitor overrideMonitor() {
+        String prop = System.getProperty("disableMetricsJmx");
+        if(prop != null) {
+            if(prop.equals("true")) {
+                return NoOpMonitor.INSTANCE;
+            } else {
+                return null;
+            }
+        } else {
+            return null;
+        }
     }
 
     @Override
@@ -59,4 +82,34 @@ public class JmxMetricsMonitor extends MetricsMonitor {
         super.close();
     }
 
+    @Override
+    public Meter newMeter(String name) {
+        return overrideMonitor == null ? super.newMeter(name) : overrideMonitor.newMeter(name);
+    }
+
+    @Override
+    public Counter newCounter(String name) {
+        return overrideMonitor == null ? super.newCounter(name) : overrideMonitor.newCounter(name);
+    }
+
+    @Override
+    public Timer newTimer(String name) {
+        return overrideMonitor == null ? super.newTimer(name) : overrideMonitor.newTimer(name);
+    }
+
+    @Override
+    public TimerPair newTimerPair(String name) {
+        return overrideMonitor == null ? super.newTimerPair(name) : overrideMonitor.newTimerPair(name);
+    }
+
+    @Override
+    public <T> Gauge<T> newGauge(String name, Supplier<T> gauge) {
+        return overrideMonitor == null ? super.newGauge(name, gauge) : overrideMonitor.newGauge(name, gauge);
+    }
+
+    @Override
+    public Histogram newHistogram(String name) {
+        return overrideMonitor == null ? super.newHistogram(name) : overrideMonitor.newHistogram(name);
+
+    }
 }
