93807a7231b972f624f047bc705a3a12a1f71cbc
Janecek Jakub <janecek@avast.com>
2016-10-03 12:53:09 +0200

    Add support for timing blocks and futures in Scala API

--- scala-api/src/main/scala/com/avast/metrics/scalaapi/Timer.scala
+++ scala-api/src/main/scala/com/avast/metrics/scalaapi/Timer.scala
@@ -4,8 +4,16 @@ import java.time.Duration
 
 import com.avast.metrics.api.Timer.TimeContext
 
+import scala.concurrent.{ExecutionContext, Future}
+
 trait Timer extends Metric {
 
   def start(): TimeContext
+
   def update(duration: Duration): Unit
+
+  def time[A](block: => A): A
+
+  def time[A](future: => Future[A])(implicit ec: ExecutionContext): Future[A]
+
 }
