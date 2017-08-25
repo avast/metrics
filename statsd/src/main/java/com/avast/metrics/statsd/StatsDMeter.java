fdef09eac2a83a3cf116b92eccefd8a5ad70d9a2
Alena Varkočková <varkockova@avast.com>
2017-08-25 13:18:18 +0200

    Sampling for statsd monitor

--- statsd/src/main/java/com/avast/metrics/statsd/StatsDMeter.java
+++ statsd/src/main/java/com/avast/metrics/statsd/StatsDMeter.java
@@ -8,12 +8,18 @@ import java.util.concurrent.atomic.AtomicLong;
 public class StatsDMeter implements Meter {
     private final StatsDClient client;
     private final String name;
+    private double sampleRate;
 
     private final AtomicLong marks = new AtomicLong(0);
 
     StatsDMeter(final StatsDClient client, final String name) {
+        this(client, name, 1.0);
+    }
+
+    StatsDMeter(final StatsDClient client, final String name, double sampleRate) {
         this.client = client;
         this.name = name;
+        this.sampleRate = sampleRate;
     }
 
     @Override
@@ -24,13 +30,13 @@ public class StatsDMeter implements Meter {
     @Override
     public void mark() {
         marks.incrementAndGet();
-        client.count(name, 1);
+        client.count(name, 1, sampleRate);
     }
 
     @Override
     public void mark(final long n) {
         marks.addAndGet(n);
-        client.count(name, n);
+        client.count(name, n, sampleRate);
     }
 
     @Override
