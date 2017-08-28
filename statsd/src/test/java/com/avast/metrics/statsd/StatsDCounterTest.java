6c814903407a0bac9f8c4345183aa55a2e8ac735
Alena Varkockova <varkockova.a@gmail.com>
2017-08-28 18:15:44 +0200

    Better tests for statsd client

--- statsd/src/test/java/com/avast/metrics/statsd/StatsDCounterTest.java
+++ statsd/src/test/java/com/avast/metrics/statsd/StatsDCounterTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.*;
 public class StatsDCounterTest {
 
     @Test
-    public void testCounts() {
+    public void testCounterValue() {
         final StatsDClient client = mock(StatsDClient.class);
 
         final String name = TestUtils.randomString();
@@ -29,8 +29,62 @@ public class StatsDCounterTest {
 
         counter.dec(8);
 
-        verify(client, times(10)).count(Matchers.eq(name), Matchers.anyLong(), Matchers.<String>anyVararg());
-
         assertEquals(-2, counter.count());
+    }
+
+    @Test
+    public void incrementCallsCountOnClient() {
+        final StatsDClient client = mock(StatsDClient.class);
+
+        final String name = TestUtils.randomString();
+
+        final StatsDCounter counter = new StatsDCounter(client, name);
+
+        for (int i = 1; i <= 5; i++) {
+            counter.inc();
+        }
+
+        verify(client, times(5)).count(Matchers.eq(name), Matchers.anyLong(), Matchers.anyDouble(), Matchers.<String>anyVararg());
+    }
+
+    @Test
+    public void decrementCallsCountOnClient() {
+        final StatsDClient client = mock(StatsDClient.class);
+
+        final String name = TestUtils.randomString();
+
+        final StatsDCounter counter = new StatsDCounter(client, name);
+
+        for (int i = 1; i <= 3; i++) {
+            counter.dec();
+        }
+
+        verify(client, times(3)).count(Matchers.eq(name), Matchers.anyLong(), Matchers.anyDouble(), Matchers.<String>anyVararg());
+    }
+
+    @Test
+    public void incrementWithMultipleCallsCountOnClient() {
+        final StatsDClient client = mock(StatsDClient.class);
+
+        final String name = TestUtils.randomString();
+
+        final StatsDCounter counter = new StatsDCounter(client, name);
+
+        counter.inc(4);
+
+        verify(client, times(1)).count(Matchers.eq(name), eq(4L), Matchers.anyDouble(), Matchers.<String>anyVararg());
+    }
+
+    @Test
+    public void samplingForwardedToClient() {
+        final StatsDClient client = mock(StatsDClient.class);
+
+        final String name = TestUtils.randomString();
+
+        final StatsDCounter counter = new StatsDCounter(client, name, 0.1);
+
+        counter.inc(4);
+
+        verify(client, times(1)).count(Matchers.eq(name), eq(4L), eq(0.1), Matchers.<String>anyVararg());
     }
 }
