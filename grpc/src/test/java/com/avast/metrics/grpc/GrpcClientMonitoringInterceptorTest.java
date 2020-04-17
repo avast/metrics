8594377eebcd458c082a87adf0da81c31cf84c37
Ondra Pelech <ondra.pelech@gmail.com>
2020-04-17 23:38:00 +0200

    fix tests

--- grpc/src/test/java/com/avast/metrics/grpc/GrpcClientMonitoringInterceptorTest.java
+++ grpc/src/test/java/com/avast/metrics/grpc/GrpcClientMonitoringInterceptorTest.java
@@ -31,11 +31,11 @@ public class GrpcClientMonitoringInterceptorTest {
 
         final Monitor monitor = mock(Monitor.class);
         final Timer timer = mock(Timer.class);
-        when(monitor.newTimer("TestApiService_GetSuccesses")).thenReturn(timer);
+        when(monitor.newTimer("TestApiService_Get_Successes")).thenReturn(timer);
         doNothing().when(timer).update(Matchers.eq(Duration.ofMillis(42)));
 
         AtomicReference<Supplier<Integer>> currentCallsSupplier = new AtomicReference<>();
-        when(monitor.newGauge(eq("TestApiService_GetCurrent"), any())).thenAnswer(invocation -> {
+        when(monitor.newGauge(eq("TestApiService_Get_Current"), any())).thenAnswer(invocation -> {
             currentCallsSupplier.set(invocation.getArgumentAt(1, Supplier.class));
             return null;
         });
@@ -81,11 +81,11 @@ public class GrpcClientMonitoringInterceptorTest {
 
         final Monitor monitor = mock(Monitor.class);
         final Timer timer = mock(Timer.class);
-        when(monitor.newTimer("TestApiService_GetFailures")).thenReturn(timer);
+        when(monitor.newTimer("TestApiService_Get_FatalServerFailures")).thenReturn(timer);
         doNothing().when(timer).update(Matchers.eq(Duration.ofMillis(42)));
 
         AtomicReference<Supplier<Integer>> currentCallsSupplier = new AtomicReference<>();
-        when(monitor.newGauge(eq("TestApiService_GetCurrent"), any())).thenAnswer(invocation -> {
+        when(monitor.newGauge(eq("TestApiService_Get_Current"), any())).thenAnswer(invocation -> {
             currentCallsSupplier.set(invocation.getArgumentAt(1, Supplier.class));
             return null;
         });
